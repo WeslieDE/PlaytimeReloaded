580f099b3e9153b3981ee2d1743f04fd8b9cf944
Chris <christopher.latza@gmail.com>
2016-09-15 20:16:32 +0200

    Add license to all files

--- src/li/inc/PlaytimeReloaded/DataStore/Config.java
+++ src/li/inc/PlaytimeReloaded/DataStore/Config.java
@@ -1,3 +1,23 @@
+/*
+* Copyright (c) 2016
+* See CONTRIBUTORS.TXT for a full list of copyright holders.
+*
+* This file is part of the spigot Minecraft server plugin 'PlaytimeReloaded'.
+*
+* PlaytimeReloaded is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* PlaytimeReloaded is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with PlaytimeReloaded.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package li.inc.PlaytimeReloaded.DataStore;
 
 import org.bukkit.configuration.file.FileConfiguration;
